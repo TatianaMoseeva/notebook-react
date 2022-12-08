4456b3c49e99e8fd91a5479d911f0423fe2cd582
Tatiana <tmoseeva@hotmail.com>
2022-12-08 11:12:25 +1100

    test styled-components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,10 @@
 
+import styled from 'styled-components';
 
+const Button = styled.button`
+    background-color: #D67D70;
+    border: none;
+    border-radius: 4px;`
 
 function Header() {
     return ( 
@@ -7,7 +12,8 @@ function Header() {
             <h1 className="title">My notes</h1>
                 <div className="actions-bar">
                     <input className="search-input" type="search" role="searchbox" placeholder="Search"></input>
-                    <button className="add-btn"><i className="fa-solid fa-plus"></i> Add note</button>
+                    <Button>Test</Button>
+                    {/* <button className="add-btn"><i className="fa-solid fa-plus"></i> Add note</button> */}
                 </div>
         </header>
 
