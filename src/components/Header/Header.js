46400f021cd1b26a56a222945fa5fb071adb5609
Tatiana <tmoseeva@hotmail.com>
2022-12-08 11:46:17 +1100

    add css to elements using styled-components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,70 @@
 
 import styled from 'styled-components';
 
-const Button = styled.button`
-    background-color: #D67D70;
-    border: none;
-    border-radius: 4px;`
+const TopContent = styled.header`
+    margin: 40px auto;
+    padding: 30px;
+    width: 100%;
+    height: 170px;
+    background: #6364A5;
+    box-shadow: 0px 6px 16px #D7D7D7;
+    border-radius: 4px;
+    h1 {
+        margin: 0;
+        padding: 0;
+        color: #fff;
+        font-weight: 400;
+        font-size: 48px;
+        line-height: 58px;
+    }
+`;
+
+const ActionsBar = styled.div`
+    display: grid;
+    grid-template-columns: auto auto;
+    gap: 50px;
+    align-items: end;
+    input {
+        margin-top: 20px;
+        padding: 10px 20px;
+        width: 100%;
+        height: 40px;
+        background: #FFFFFF;
+        border: none;
+        border-radius: 4px;
+
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 23px;
+    }
+    button {
+        width: 100%;
+        height: 40px;
+        background-color: #D67D70;
+        border: none;
+        border-radius: 4px;
+        
+        color: #fff;
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 23px;
+        text-transform: capitalize;
+        transition: all .3s;
+        :hover {
+            background-color: #c27165;
+        }
+    }
+`;
 
 function Header() {
     return ( 
-        <header>
-            <h1 className="title">My notes</h1>
-                <div className="actions-bar">
-                    <input className="search-input" type="search" role="searchbox" placeholder="Search"></input>
-                    <Button>Test</Button>
-                    {/* <button className="add-btn"><i className="fa-solid fa-plus"></i> Add note</button> */}
-                </div>
-        </header>
+        <TopContent>
+            <h1>My notes</h1>
+                <ActionsBar>
+                    <input type="search" role="searchbox" placeholder="Search"></input>
+                    <button><i className="fa-solid fa-plus"></i> Add note</button>
+                </ActionsBar>
+        </TopContent>
 
     );
   }
--- src/components/NotesField/NotesField.js
+++ src/components/NotesField/NotesField.js
@@ -1,22 +1,67 @@
+import styled from 'styled-components';
+
+const Field = styled.main`
+    display: grid;
+    grid-template-columns: 200px auto;
+    gap: 30px;
+    nav {
+        display: grid;
+        grid-template-columns: auto;
+        grid-auto-rows: auto;
+        gap: 20px;
+        button {
+            width: 100%;
+            height: 40px;
+            background-color: #6364A5;
+            box-shadow: 0px 6px 16px #D7D7D7;
+            border: none;
+            border-radius: 4px;
+
+            color: #fff;
+            font-weight: 400;
+            font-size: 20px;
+            line-height: 23px;
+            transition: all .3s;
+            :hover {
+                background-color: #565794;
+            }
+            &.active {
+                background-color: #D67D70;
+                :hover {
+                    background-color: #c27165;
+                }
+            }
+        }
+    }
+    textarea {
+        padding: 20px;
+        resize: none;
+        background: #F2F2F2;
+        border: 1px solid #D7D7D7;
+        border-radius: 4px;
+
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 23px;
+    }
+`;
+
 function NotesField() {
     return ( 
-        <main className="notes-field">
-            <nav className="nav-buttons">
-                <button className="nav-btn active">Title</button>
-                <button className="nav-btn">Cake Recipe</button>
-                <button className="nav-btn">Lecture notes</button>
-                <button className="nav-btn">Some ideas</button>
-                <button className="nav-btn">Birthdays</button>
-                <button className="nav-btn">My expences</button>
+        <Field>
+            <nav>
+                <button className="active">Title</button>
+                <button>Cake Recipe</button>
+                <button>Lecture notes</button>
+                <button>Some ideas</button>
+                <button>Birthdays</button>
+                <button>My expences</button>
             </nav>
-            <textarea className="textarea" placeholder="Note"></textarea>
+            <textarea placeholder="Note"></textarea>
 
-            {/* <i className="fa-regular fa-copy"></i>
-            <i className="fa-regular fa-trash-can"></i> */}
-
-
-        </main>
+        </Field>
     );
   }
-  
+            /* <i className="fa-regular fa-copy"></i>
+            <i className="fa-regular fa-trash-can"></i> */
   export default NotesField;
